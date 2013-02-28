b6a5b3423e5c43b1b1cd7defaf4b2082fb878297
metaleap <philipp.schumann@gmail.com>
2013-02-28 22:39:34 +0800

    ..

--- util/program-manager.go
+++ util/program-manager.go
@@ -9,31 +9,31 @@ import (
 const progManCap = 16
 
 type ProgramManager struct {
-	Programs []Program
+	All []Program
 
 	names map[string]int
 }
 
 func (me *ProgramManager) Init() {
-	me.Programs = make([]Program, 0, progManCap)
+	me.All = make([]Program, 0, progManCap)
 	me.names = make(map[string]int, progManCap)
 }
 
 func (me *ProgramManager) Dispose() {
-	for i := 0; i < len(me.Programs); i++ {
-		me.Programs[i].Dispose()
+	for i := 0; i < len(me.All); i++ {
+		me.All[i].Dispose()
 	}
 }
 
 func (me *ProgramManager) AddNew(name string) (prog *Program) {
-	if len(me.Programs) == cap(me.Programs) {
-		nuProgs := make([]Program, len(me.Programs), cap(me.Programs)+progManCap)
-		copy(nuProgs, me.Programs)
-		me.Programs = nuProgs
+	if len(me.All) == cap(me.All) {
+		nuProgs := make([]Program, len(me.All), cap(me.All)+progManCap)
+		copy(nuProgs, me.All)
+		me.All = nuProgs
 	}
-	index := len(me.Programs)
-	me.Programs = append(me.Programs, Program{})
-	prog = &me.Programs[index]
+	index := len(me.All)
+	me.All = append(me.All, Program{})
+	prog = &me.All[index]
 	prog.Init(name)
 	me.names[name] = index
 	return
@@ -41,20 +41,20 @@ func (me *ProgramManager) AddNew(name string) (prog *Program) {
 
 func (me *ProgramManager) Get(name string) (prog *Program) {
 	if i, ok := me.names[name]; ok {
-		prog = &me.Programs[i]
+		prog = &me.All[i]
 	}
 	return
 }
 
 func (me *ProgramManager) MakeProgramsFromRawSources(defines map[string]interface{}, forceAll bool, forceSome ...string) (dur time.Duration, progsMade []bool, err error) {
-	progsMade = make([]bool, len(me.Programs))
+	progsMade = make([]bool, len(me.All))
 	timeStart := time.Now()
-	for i := 0; i < len(me.Programs); i++ {
-		if me.Programs[i].GlHandle == 0 || forceAll || ustr.IsInSlice(forceSome, me.Programs[i].Name) {
+	for i := 0; i < len(me.All); i++ {
+		if me.All[i].GlHandle == 0 || forceAll || ustr.IsInSlice(forceSome, me.All[i].Name) {
 			progsMade[i] = true
-			me.Programs[i].Create()
-			if err = me.Programs[i].CompileAndLinkShaders(defines); err != nil {
-				me.Programs[i].Dispose()
+			me.All[i].Create()
+			if err = me.All[i].CompileAndLinkShaders(defines); err != nil {
+				me.All[i].Dispose()
 				return
 			}
 		}
